3beba251d33597439eb3e775f769844e5d704ee5
ValentainFomkin <valentain1625@mail.ru>
2022-12-19 16:15:04 +0300

    completed hw4

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -1,10 +1,4 @@
-import React, {
-    ChangeEvent,
-    DetailedHTMLProps,
-    InputHTMLAttributes,
-    KeyboardEvent,
-    ReactNode,
-} from 'react'
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, KeyboardEvent, ReactNode,} from 'react'
 import s from './SuperInputText.module.css'
 
 // тип пропсов обычного инпута
@@ -48,11 +42,8 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
         onEnter() // то вызвать его
     }
 
-    const finalSpanClassName = s.error
-        + (spanClassName ? ' ' + spanClassName : '')
-    const finalInputClassName = s.input
-        + (error ? ' ' + s.errorInput : ' ' + s.superInput)
-        + (className ? ' ' + s.className : '') // задача на смешивание классов
+    const finalSpanClassName = s.error + (spanClassName ? ' ' + spanClassName : '')
+    const finalInputClassName = s.input + (error ? ' ' + s.errorInput : ' ' + s.superInput) + (className ? ' ' + s.className : '') // задача на смешивание классов
 
     return (
         <div className={s.inputWrapper}>
