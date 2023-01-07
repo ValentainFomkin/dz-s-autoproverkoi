8c11e107c4daa1a350b3331a6b6d49612b6bbd40
ValentainFomkin <valentain1625@mail.ru>
2023-01-07 18:26:35 +0300

    completed hw7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -44,7 +44,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                     // name, checked, value делают студенты
                     name={name}
                     checked={o === value}
-                    value={o}
+                    value={o.id}
                     onChange={onChangeCallback}
                     {...restProps}
                 />
