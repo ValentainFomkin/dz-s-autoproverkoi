b87a5f52c75f52e65b2fc531bb96dec391d17120
ValentainFomkin <valentain1625@mail.ru>
2023-01-11 16:23:10 +0300

    completed hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,44 @@
-import {UserType} from '../HW8'
+import {UserType} from "../HW8";
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+const initialPeople: UserType[] = [
+    // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
+    {_id: 0, name: 'Кот', age: 3},
+    {_id: 1, name: 'Александр', age: 66},
+    {_id: 2, name: 'Коля', age: 16},
+    {_id: 3, name: 'Виктор', age: 44},
+    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 5, name: 'Ирина', age: 55},
+]
+
+export const homeWorkReducer = (state = initialPeople, action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 
-            return state // need to fix
+            if (action.payload === 'up') {
+                return state.sort(function (a, b) {
+                    let nameA = a.name.toLowerCase()
+                    let nameB = b.name.toLowerCase()
+                    return nameA > nameB ? 1 : -1
+
+                })
+            }
+            if (action.payload === 'down') {
+                return state.sort(function (a, b) {
+                    let nameA = a.name.toLowerCase()
+                    let nameB = b.name.toLowerCase()
+                    return nameA < nameB ? 1 : -1
+                })
+            }
+            return 0
+            // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(e => e.age >= action.payload) // need to fix
         }
         default:
             return state
