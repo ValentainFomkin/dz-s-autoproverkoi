9cb3433712cf8fb61698b805bc9a5793a859d799
ValentainFomkin <valentain1625@mail.ru>
2023-02-18 20:28:58 +0300

    completed hw10

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -13,9 +13,11 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 2))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: any) => {
+        setValue1(event.eventPhase + value)
+        setValue2(event.eventPhase + value)
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
 
@@ -28,6 +30,8 @@ function HW11() {
                     <div className={s.wrapper}>
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
+                            value={value1}
+                            onChange={change}
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
 
@@ -36,6 +40,8 @@ function HW11() {
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
+                            onChange={change}
+                            value={value2}
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
